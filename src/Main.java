276f3a6d00b66212049ce00957e8328a6872c3a1
Madina <madina-khat@yandex.ru>
2022-10-14 22:25:38 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int clientOS = 0;
-        int clientAndroid = 1;
-        if (clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        if (clientAndroid == 1) {
-            System.out.println("Установите версию приложения для Android по ссылке");
 
-    }
-}}}
+            int clientOS = 3; // 0 — iOS, 1 — Android
+            int clientDeviceYear = 2014;
+            if (clientDeviceYear >= 2015 && clientOS == 0) {
+                System.out.println("Установите обычную версию приложения для iOS по ссылке.");
+            } else if (clientDeviceYear <= 2015 && clientOS == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+            } else if (clientDeviceYear >= 2015 && clientOS == 1) {
+                System.out.println("Установите обычную версию приложения для Android по ссылке.");
+            } else if (clientDeviceYear <= 2015 && clientOS == 1) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+            } else System.out.println("К сожалению, для Вашего устройства нет приложения.");
+        } }
