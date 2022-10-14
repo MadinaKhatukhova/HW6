d07fcd3747011baf8a408f0a1e3f44ba5aeae870
Madina <madina-khat@yandex.ru>
2022-10-14 22:51:31 +0300

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-    int year = 2100;
-    boolean leapYear = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
-    if (leapYear) {
-        System.out.println(year + " год является високосным.");
-    } else  {
-        System.out.println(year + " год не является високосным.");
-    }
-}}
+        int deliveryDistance = 100;
+        int deliveryDays = 1;
+        int distanceAddDays = 40; // Шаг расстояния, при котором увеличивается доставка на СУТКИ
+        int remainingDistance = deliveryDistance - 20; // Если разница <= 0, то выведем СУТКИ на доставку, если больше, то ИНАЧЕ
+        System.out.print("Потребуется дней: ");
+        if (remainingDistance <= 0) {
+            System.out.println(deliveryDays);
+        } else if (remainingDistance % distanceAddDays != 0) {
+            System.out.println((deliveryDays += remainingDistance / distanceAddDays + 1));
+        } else {
+            System.out.println((deliveryDays += remainingDistance / distanceAddDays));
+        }
+    }}
 
 
 
