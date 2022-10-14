4bd36a4f91f0ccb3955d72afaa00454f4dcf14d4
Madina <madina-khat@yandex.ru>
2022-10-14 22:48:52 +0300

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-            int clientOS = 3; // 0 — iOS, 1 — Android
-            int clientDeviceYear = 2014;
-            if (clientDeviceYear >= 2015 && clientOS == 0) {
-                System.out.println("Установите обычную версию приложения для iOS по ссылке.");
-            } else if (clientDeviceYear <= 2015 && clientOS == 0) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
-            } else if (clientDeviceYear >= 2015 && clientOS == 1) {
-                System.out.println("Установите обычную версию приложения для Android по ссылке.");
-            } else if (clientDeviceYear <= 2015 && clientOS == 1) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-            } else System.out.println("К сожалению, для Вашего устройства нет приложения.");
-        } }
+    int year = 2100;
+    boolean leapYear = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
+    if (leapYear) {
+        System.out.println(year + " год является високосным.");
+    } else  {
+        System.out.println(year + " год не является високосным.");
+    }
+}}
+
+
+
