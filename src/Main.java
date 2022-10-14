8339465ca974c7455a26abc08dbbbef89aaa54c8
Madina <madina-khat@yandex.ru>
2022-10-14 23:21:10 +0300

    Задача 8

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,44 @@
+import java.text.DecimalFormat;
+
 public class Main {
     public static void main(String[] args) {
 
-        int age = 25;
-        int standartAge = 23;
-        int salary = 30_000;
-        int standardSalary = 50_000;
-        int moreStandartSalary = 80_000;
-        int limitYoung = salary * 2;
-        int limitAdult = salary * 3;
-        float creditLimit;
-        System.out.print("Мы готовы выдать Вам кредитную карту с лимитом ");
-        if (age >= standartAge && salary >= moreStandartSalary) {
-            creditLimit = limitAdult * 1.5f;
-        } else if (age >= standartAge && salary >= standardSalary) {
-            creditLimit = limitAdult * 1.2f;
-        } else if (age >= standartAge && salary < standardSalary) {
-            creditLimit = limitAdult;
-        } else if (age < standartAge && salary >= moreStandartSalary) {
-            creditLimit = limitYoung * 1.5f;
-        } else if (age < standartAge && salary >= standardSalary) {
-            creditLimit = limitYoung * 1.2f;
+        int age = 30;
+        int salary = 70_000;
+        int standartSalary = 80_000;
+        int wantedSum = 330_000;
+        int maxMonthPayment = salary / 2;
+        float monthPayment;
+        float baseRate = 0.1f;
+        float baseRateStandartSalary = baseRate - 0.007f;
+        float baseRateLess23Age = baseRate + 0.01f;
+        float baseRateLess30Age = baseRate + 0.005f;
+        float baseRateLess23AgeStandartSalary = baseRateLess23Age - 0.007f;
+        float baseRateLess30AgeStandartSalary = baseRateLess30Age - 0.007f;
+        System.out.print("Максимальный платеж при ЗП " + salary + " руб. равен " + maxMonthPayment + " руб. Платеж по кредиту ");
+        if (age >= 30 && salary < standartSalary) {
+        } else if (age >= 30 && salary >= standartSalary) {
+            baseRate = baseRateStandartSalary;
+        } else if (age >= 23 && salary >= standartSalary) {
+            baseRate = baseRateLess30AgeStandartSalary;
+        } else if (age >= 23 && salary < standartSalary) {
+            baseRate = baseRateLess30Age;
+        } else if (age < 23 && salary >= standartSalary) {
+            baseRate = baseRateLess23AgeStandartSalary;
         } else {
-            creditLimit = limitYoung;
+            baseRate = baseRateLess23Age;
         }
-        System.out.println(creditLimit + " рублей.");
-    }}
+        monthPayment = (wantedSum * baseRate + wantedSum) / 12;
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        String result = decimalFormat.format(monthPayment);
+        System.out.print(result + " рублей. ");
+        if (monthPayment >= maxMonthPayment) {
+            System.out.println("Не одобрено.");
+        } else {
+            System.out.println("Одобрено.");
 
 
+
+
+
+        }}}
